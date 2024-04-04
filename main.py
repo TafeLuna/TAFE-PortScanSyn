b09f52c76bfe341634619f7afb7b2c899150aa6d
Luna <4h7menlv@duck.com>
2024-04-04 11:35:20 +1100

    Added pyping for host check

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 #
 
 import nmap3 # python3-nmap==1.6.0 
-
+from pythonping import ping
 PORT_MIN = 80
 PORT_MAX = 443
 TARGET = "127.0.0.1" # To be swapped out with target and cannot be a domain
 
 def is_host_alive():
-    return pythonping.ping(target=TARGET,count=2,timeout=5).success()
+    return ping(target=TARGET,count=2,timeout=5).success()
 
 def scan_ports():
     # Using a SYN scan instead of TCP for speed
